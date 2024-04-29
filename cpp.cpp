3df0adda05a99eaa5682f8828c041e52fb6f8b19
xc1ldy <168462900+xc1ldy@users.noreply.github.com>
2024-04-29 19:37:29 +0300

    оптимизированно

--- cpp.cpp
+++ cpp.cpp
@@ -4,5 +4,10 @@ using namespace std;
 int main() {
     int number;
     cin >> number;
-    cout << (number % 2 == 0 ? "Четное" : "Нечетное") << endl;
+    if (number % 2 == 0) {
+        cout << "even" << endl;
+    }
+    else {
+        cout << "odd" << endl;
+    }
 }
